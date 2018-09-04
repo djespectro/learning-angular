bdd5516878478503eaa3b2b5a8578312698c9ee8
mac <djespectro@hotmail.com>
2018-09-04 05:48:38 -0700

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  
+  username = 'mac';
+  isEnabledAddUser = false;
+  isEnabledInputName = false;
+
+  ngOnInit(): void {
+    setTimeout(() => {this.isEnabledAddUser=true;}, 2000);
+  }
+
+  enableInputName(){
+    this.isEnabledInputName = !this.isEnabledInputName;
+  }
+
 }
